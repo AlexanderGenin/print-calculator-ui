0b975e21c14bc3ce78ce6d8779d1cf81b78fe237
Aleksandr Genin <a.genin@macbook-a.genin>
2023-07-12 14:22:13 +0300

    Init webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: "[name].bundle.js",
+    publicPath: "/",
+  },
+  mode: "development",
+  devtool: "inline-source-map",
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Calculator",
+      template: "./template.html",
+      filename: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env"], ["@babel/preset-react"]],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    modules: ["node_modules"],
+    extensions: [".js", ".jsx", ".json"],
+  },
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+};
